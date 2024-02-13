2eedec65307b7c3751a6d022507337a7960137a5
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-13 16:54:52 +0900

    変数追加

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
 TOKEN = 'your_token_here'
 SECRET = 'your_secret_here'
 DEVICE_1 = 'your_device_id_here'
+DB_HOST = 'localhost'
+DB_USER = 'your_DB_user_here'
+DB_PASSWORD = 'your_DB_password_here'
+DB_DATABASE = 'your_DB_database_here'
+DB_SCHEMA = """
+CREATE TABLE IF NOT EXISTS sensor_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    time TIMESTAMP,
+    humidity FLOAT,
+    temperature FLOAT,
+    battery FLOAT
+);
+"""
