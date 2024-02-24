7ec3d62e9c701ab1781350d7d66e586e61dbe6cf
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-24 21:26:31 +0900

    Create switchbot_api_v11.sh

---
+++ switchbot_api_v11.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ------region------
+token="<トークン>"
+secret="<シークレット>"
+t=$(date +%s%3N)
+nonce=$(uuidgen -r)
+sign=$(echo -n ${token}${t}${nonce} | openssl dgst -sha256 -hmac ${secret} -binary | base64)
+# ------------------
+
+result=$(
+    curl -s "https://api.switch-bot.com/v1.1/devices" \
+      --header "Authorization: ${token}" \
+      --header "sign: ${sign}" \
+      --header "t: ${t}" \
+      --header "nonce: ${nonce}" \
+      --header "Content-Type: application/json; charset=utf8")
+
+echo $result
