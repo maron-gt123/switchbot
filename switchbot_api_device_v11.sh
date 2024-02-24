1d9cfa474dc4b9f296bb1efce8245eb3c80d74e7
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-25 03:20:38 +0900

    Update switchbot_api_device_v11.sh

--- switchbot_api_device_v11.sh
+++ switchbot_api_device_v11.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # ------region------
-token="<トークン>"
-secret="<シークレット>"
-t=$(date +%s%3N)
-nonce=$(uuidgen -r)
-sign=$(echo -n ${token}${t}${nonce} | openssl dgst -sha256 -hmac ${secret} -binary | base64)
+TOKEN="<トークン>"
+SECRET="<シークレット>"
+T=$(date +%s%3N)
+NONCE=$(uuidgen -r)
+SIGN=$(echo -n ${TOKEN}${T}${NONCE} | openssl dgst -sha256 -hmac ${SECRET} -binary | base64)
 DEVICEID="<デバイスID>"
 # ------------------
 
-result=$(
-    curl -s "https://api.switch-bot.com/v1.1/devices" \
-      --header "Authorization: ${token}" \
-      --header "sign: ${sign}" \
-      --header "t: ${t}" \
-      --header "nonce: ${nonce}" \
-      --header "Content-Type: application/json; charset=utf8" | jq '.')
+RESULT=$(
+    curl -s "https://api.switch-bot.com/v1.1/devices/${DEVICEID}/status" \
+      --header "Authorization: ${TOKEN}" \
+      --header "sign: ${SIGN}" \
+      --header "t: ${T}" \
+      --header "nonce: ${NONCE}" \
+      --header "Content-Type: application/json; charset=utf8")
 
-echo "$result"
+echo "$RESULT"
