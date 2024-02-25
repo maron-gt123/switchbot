9e6bf67e20f2ef122ba30a3a71719be5f5c33067
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-25 22:47:33 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 TOKEN = 'your_token_here'
 SECRET = 'your_secret_here'
 DEVICE_1 = 'your_device_id_here'
+DEVICE_2 = 'your_device_id_here'
 DB_HOST = 'localhost'
 DB_USER = 'your_DB_user_here'
 DB_PASSWORD = 'your_DB_password_here'
