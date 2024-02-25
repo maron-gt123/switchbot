b81e1f358b65f5fbf68a1be7162c54d5cdc3b991
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-25 22:02:33 +0900

    Create switchbot.py

---
+++ switchbot.py
@@ -0,0 +1,45 @@
+import json
+import time
+import hashlib
+import hmac
+import base64
+import uuid
+import requests
+from config import TOKEN, SECRET, DEVICE_1
+from rich import print
+
+nonce = uuid.uuid4()
+t = f"{time.time() * 1000:.0f}"
+string_to_sign = '{}{}{}'.format(TOKEN, t, nonce)
+
+string_to_sign = bytes(string_to_sign, 'utf-8')
+secret = bytes(SECRET, 'utf-8')
+sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
+
+#Build api header JSON
+headers = {
+    'Authorization': TOKEN,
+    'sign': str(sign, "utf-8"),
+    't': t,
+    'nonce': str(nonce),
+    'Content-Type': 'application/json',
+    "charset": 'utf8'
+}
+
+api_endpoint = f'https://api.switch-bot.com/v1.1/devices/{DEVICE_1}/status'
+
+res = requests.get(api_endpoint, headers=headers)
+
+data = res.json()
+
+device = data["body"]['deviceId']
+humidity = data["body"]["humidity"] # 湿度計
+temperature = data["body"]["temperature"] # 温度計
+battery = data["body"]["battery"] # バッテリ
+version = data["body"]["version"] # バージョン
+
+print(device)
+print(humidity)
+print(temperature)
+print(battery)
+print(version)
