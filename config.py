02a06469287b239f41463bed55af0349cc680e48
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-27 05:25:46 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,7 @@
 TOKEN = 'your_token_here'
 SECRET = 'your_secret_here'
 DEVICE_1 = 'your_device_id_here'
-DEVICE_CM_1 = 'your_device_name_here'
 DEVICE_2 = 'your_device_id_here'
-DEVICE_CM_2 = 'your_device_name_here'
 # InfluxDBの接続情報
 INFLUXDB_URL = "http://localhost:8086"  # InfluxDBのURL
 INFLUXDB_TOKEN = "your_influxdb-token_here"  # InfluxDBのアクセストークン
