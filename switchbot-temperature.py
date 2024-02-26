a32aea7cd7fde5412d6706315e00a9a6af007e9a
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2024-02-27 04:06:19 +0900

    ループ機能削除

--- switchbot-temperature.py
+++ switchbot-temperature.py
@@ -21,50 +21,47 @@ def write_to_influxdb(device, humidity, temperature, battery, version):
     point = Point("device_data").tag("device", device).field("humidity", humidity).field("temperature", temperature).field("battery", battery).field("version", version)
     write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
 
-while True:
-    nonce = uuid.uuid4()
-    t = f"{time.time() * 1000:.0f}"
-    string_to_sign = '{}{}{}'.format(TOKEN, t, nonce)
-    string_to_sign = bytes(string_to_sign, 'utf-8')
-    secret = bytes(SECRET, 'utf-8')
-    sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
+nonce = uuid.uuid4()
+t = f"{time.time() * 1000:.0f}"
+string_to_sign = '{}{}{}'.format(TOKEN, t, nonce)
+string_to_sign = bytes(string_to_sign, 'utf-8')
+secret = bytes(SECRET, 'utf-8')
+sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
 
-#Build api header JSON
-    headers = {
-        'Authorization': TOKEN,
-        'sign': str(sign, "utf-8"),
-        't': t,
-        'nonce': str(nonce),
-        'Content-Type': 'application/json',
-        "charset": 'utf8'
-    }
+# Build api header JSON
+headers = {
+    'Authorization': TOKEN,
+    'sign': str(sign, "utf-8"),
+    't': t,
+    'nonce': str(nonce),
+    'Content-Type': 'application/json',
+    "charset": 'utf8'
+}
 
-    api_endpoint = f'https://api.switch-bot.com/v1.1/devices/{DEVICE_1}/status'
+api_endpoint = f'https://api.switch-bot.com/v1.1/devices/{DEVICE_1}/status'
 
-    res = requests.get(api_endpoint, headers=headers)
+res = requests.get(api_endpoint, headers=headers)
 
-    data = res.json()
+data = res.json()
 
-    device = data["body"]['deviceId'] #デバイスID
-    humidity = data["body"]["humidity"] # 湿度計
-    temperature = data["body"]["temperature"] # 温度計
-    battery = data["body"]["battery"] # バッテリ
-    version = data["body"]["version"] # バージョン
+device = data["body"]['deviceId'] #デバイスID
+humidity = data["body"]["humidity"] # 湿度計
+temperature = data["body"]["temperature"] # 温度計
+battery = data["body"]["battery"] # バッテリ
+version = data["body"]["version"] # バージョン
 
-    # デバック用
-    #print("デバイス名")
-    #print(device)
-    #print("湿度")
-    #print(humidity)
-    #print("温度")
-    #print(temperature)
-    #print("バッテリ")
-    #print(battery)
-    #print("バージョン")
-    #print(version)
+# デバック用
+#print("デバイス名")
+#print(device)
+#print("湿度")
+#print(humidity)
+#print("温度")
+#print(temperature)
+#print("バッテリ")
+#print(battery)
+#print("バージョン")
+#print(version)
 
-    # データをInfluxDBに書き込む
-    write_to_influxdb(device, humidity, temperature, battery, version)
-    print("書き込みました")
-    # 5分待機
-    time.sleep(300)  # 300秒 = 5分
+# データをInfluxDBに書き込む
+write_to_influxdb(device, humidity, temperature, battery, version)
+print("書き込みました")
